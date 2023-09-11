df3518ed364910e92a07e5a492c3aabe9f8d8c24
Arturo Garcia <arturo.garcia.luque@gmail.com>
2023-09-11 12:32:21 +0200

    TaggingTool2 FaseII

--- TaggingTool2/Program.cs
+++ TaggingTool2/Program.cs
@@ -1,10 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using TaggingTool2;
 
+const string DefaultTagfile = "./tagdata.xlsx";
+const string DefaultSheet = "Hoja1";
+
+
+Console.WriteLine($"TagTool 2. Herramienta de Etiquetado de ficheros. Grupo Amper 2023");
 Console.WriteLine("Starting Tagging Process...");
 
-var service = new TaggingService();
-await service.Update();
+try
+{
+    var TagFile = args.Length > 0 ? args[0] : DefaultTagfile;
+    var Sheet = args.Length > 1 ? args[1] : DefaultSheet;
+
+    Console.WriteLine($"TaggingTool2 {TagFile}, {Sheet}");
+    if (File.Exists(TagFile) == false)
+    {
+        var color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("No se encuentra el fichero de descripcion < {0} >", TagFile);
+        Console.ForegroundColor = color;
+        Console.WriteLine($"Pulse ENTER para continuar...");
+        Console.ReadLine();
+        return;
+    }
+
+    var service = new TaggingService(TagFile, Sheet);
+    await service.Update();
+}
+catch (Exception ex)
+{
+    var color = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Error => {ex.Message}"/*ex.ToString()*/);
+    Console.ForegroundColor = color;
+}
 
 Console.WriteLine("Tagging Process Finished... ENTER for exit.");
 Console.ReadLine();
--- TaggingTool2/TaggingService.cs
+++ TaggingTool2/TaggingService.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml.Wordprocessing;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace TaggingTool2
 {
     public class TaggingService
     {
-        class FileInfo
+        class ItemFileInfo
         {
-            public string Size { get; set; } = "";
+            public long Size { get; set; } = 0;
             public string Date { get; set; } = "";
             public string Hash { get; set; } = "";
         }
@@ -39,7 +43,7 @@ namespace TaggingTool2
                     {
                         var mode = row.Cell(1).Value.ToString();
                         var findFrom = row.Cell(2).Value.ToString();
-                        var file = row.Cell(3).Value.ToString() + "." + row.Cell(4).Value.ToString();
+                        var file = nameOfFile(row.Cell(3).Value.ToString(), row.Cell(4).Value.ToString());
 
                         var fileInfo = await Find(findFrom, file, mode);
                         PrintInfo(findFrom, file, mode, fileInfo.Hash);
@@ -52,22 +56,110 @@ namespace TaggingTool2
                 workbook.Save();
             }
         }
-        private async Task<FileInfo> Find(string from, string file, string mode)
+        private async Task<ItemFileInfo> Find(string from, string file, string mode)
         {
-            await Task.Delay(100);
-            return new FileInfo() { Hash = "Not Found" };
+            //await Task.Delay(100);
+            try 
+            {
+                var found = Directory.GetFiles(/*dirFrom*/from, file, SearchOption.AllDirectories).FirstOrDefault();
+                if (found == null)
+                {
+                    return new ItemFileInfo() { Hash = "Not Found" };
+                }
+                FileInfo fileInfo = new FileInfo(found);
+                return await ItemFileInfoGet(fileInfo, mode);
+            }
+            catch 
+            { 
+                return new ItemFileInfo() { Hash = "Not Found" };
+            }
         }
-        private void PrintInfo (string findFrom, string file, string mode, string hash)
+        private async Task<ItemFileInfo> ItemFileInfoGet(FileInfo fileInfo, string mode)
+        {
+            var itemFileInfo = new ItemFileInfo();
+            itemFileInfo.Size = fileInfo.Length;
+            itemFileInfo.Hash = GetHash(fileInfo.FullName);
+            itemFileInfo.Date = GetDate(fileInfo, mode);
+            await Task.Delay(50);
+            return itemFileInfo;
+        }
+        void PrintInfo (string findFrom, string file, string mode, string hash)
         {
             var color = Console.ForegroundColor;
-            Console.ForegroundColor = hash == "Not Found" ? ConsoleColor.Red : ConsoleColor.White;
+            Console.ForegroundColor = hash == "Not Found" ? ConsoleColor.Red : ConsoleColor.Green;
 
-            file = file.Length > 48 ? file.Substring(0, 48) + " ..." : file;
-            findFrom = findFrom.Length > 32 ? findFrom.Substring(0, 32) + " ..." : findFrom;
+            file = file.Length > 30 ? file.Substring(0, 30) + "..." : file;
+            findFrom = findFrom.Length > 20 ? findFrom.Substring(0, 20) + "..." : findFrom;
+            hash = hash.Length > 30 ? hash.Substring(0, 30) + "..." : hash;
 
-            Console.WriteLine($"Processing {file} FROM {findFrom}, MODE {mode} => {hash}");
+            Console.WriteLine($" Processing {file, 33} FROM {findFrom, 23}, MODE {mode} => {hash, 33}");
 
             Console.ForegroundColor = color;
+        }
+        string nameOfFile(string name, string ext) => ext == "" ? name : name + "." + ext;
+        string GetHash(string filePath)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                using (var stream = System.IO.File.OpenRead(filePath))
+                {
+                    return BitConverter.ToString(sha256.ComputeHash(stream)).Replace("-", string.Empty).ToUpper();
+                }
+            }
+        }
+        string GetDate(FileInfo fileInfo, string mode)
+        {
+            switch (mode)
+            {
+                case "0":
+                    return fileInfo.LastWriteTime.ToShortDateString();
+                case "1": // Buscar "NUCLEODF 2017-01-26 16:35:50" Grupo 1.
+                case "2":
+                    return DateSearch(fileInfo.FullName, mode, "NUCLEODF ((19|20)\\d\\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]))", 1);
+                case "3": // Buscar "Driver PCM 82xx MCC. $Revision: 1.3 $ (Dec 20 2016 18:13:11)", Grupo 3
+                    return DateSearch(fileInfo.FullName, mode, "Revision:(.*)(\\((.*)\\))", 3);
+                case "4": // Buscar "05/01/2017, 15:39 <291328>" Grupo 1
+                    return DateSearch(fileInfo.FullName, mode, "([0-9][0-9][\\/][0-9][0-9][\\/](19|20)[1-9][1-9]), [0-9][0-9]:[0-9][0-9] <", 1);
+                case "5": // Buscar "NUCLEODF 2017-01-26 16:35:50" Grupo 1, 2 veces,
+                    return DateSearch(fileInfo.FullName, mode, "NUCLEODF ((19|20)\\d\\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01]))", 1, 2);
+            }
+            return "Date not Found";
+        }
+        string DateSearch(string filePath, string mode, string pattern, int grp, int rep = 0)
+        {
+            var found = Search(filePath, pattern, grp, rep);
+            if (found != "")
+            {
+                return DateNormalize(found, mode);
+            }
+            return "Date not Found";
+        }
+        string Search(string filePath, string pattern, int grp, int rep = 0)
+        {
+            var data = System.Text.Encoding.UTF8.GetString(System.IO.File.ReadAllBytes(filePath));
+            MatchCollection matches = Regex.Matches(data, pattern);
+            if (matches.Count > rep)
+            {
+                return matches[rep].Groups[grp].Value;
+            }
+            return "";
+        }
+        string DateNormalize(string inputDate, string mode)
+        {
+            switch (mode)
+            {
+                case "1": // AAAA-MM-dd
+                case "2":
+                case "5":
+                    return DateTime.ParseExact(inputDate, format: "yyyy-MM-dd", provider: CultureInfo.InvariantCulture)
+                        .ToShortDateString();
+                case "3": // Feb 20 202 HH:mm:ss
+                    return DateTime.Parse(inputDate)
+                        .ToShortDateString();
+                case "4":
+                    return inputDate;
+            }
+            return $"({inputDate}) => Date Format Error";
         }
     }
 }
